ba3f7897f333114ce01191be3216faf06c29d690
Antoine Maillard <antoine.maillard@epitech.eu>
2021-10-10 01:10:27 +0200

    update code arcitecture, add router

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./../../styles/components/Home.scss";
+
+const Home = () => {
+  return (
+    <div>
+        HOME PAGE
+    </div>
+  );
+}
+
+export default Home;
