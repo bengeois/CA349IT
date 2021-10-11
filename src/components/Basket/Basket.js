f67c68eed8b5bcd7a53282bf57c5800ee0482e66
Antoine Maillard <antoine.maillard@epitech.eu>
2021-10-12 01:51:42 +0200

    feature(basket): ready for info box on basket

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -7,11 +7,17 @@ const Basket = () => {
     const itemsCount = useSelector((state) => state.basket.itemsCount);
 
     return (
-        <div className="basket-box">
-            <span className="basket-article-nb">{itemsCount}</span>
-            <div className="basket-logo-box">
-                <img className="basket-img" src="/img/basket.png" alt="Basket" />
-                Basket
+        <div className="basket">
+            <div className="basket-box">
+                <span className="basket-article-nb">{itemsCount}</span>
+                <div className="basket-logo-box">
+                    <img className="basket-img" src="/img/basket.png" alt="Basket" />
+                    Basket
+                </div>
+            </div>
+            <div className="basket-info-box">
+                <div className="basket-items-box">
+                </div>
             </div>
         </div>
     );
