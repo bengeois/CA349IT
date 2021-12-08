4efe42bd25e9a3d06e50a29119dc8092bb500fe3
Benjamin Bourgeois <benjishift@gmail.com>
2021-12-08 15:34:06 +0000

    :tada: added new templates

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import BasketReducer from './../components/Basket/BasketSlice';
-
-export const store = configureStore({
-  reducer: {
-    basket: BasketReducer
-  }
-});
